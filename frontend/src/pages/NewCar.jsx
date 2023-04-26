bfd0bdd737e0183e28ab9ac51b970f4c034c9b8c
Adam Nowatkowski <adam.nowatkowski@lppsa.com>
2023-04-26 22:21:47 +0200

    progress

--- frontend/src/pages/NewCar.jsx
+++ frontend/src/pages/NewCar.jsx
@@ -1,9 +1,79 @@
 import React from 'react'
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Layout from '../components/Layout';``
+import FormInput from '../components/FormInput';
+
 
 const NewCar = () => {
+
+const emptyCar = {
+  "brand":"",
+  "make":"",
+  "year":null,
+  "cm3":null,
+  "price":null
+}
+
+const inputs = [
+    {
+        id:"brand",
+        name:"brand",
+        type:"text",
+        placeholder:"Brand",
+        label:"Brand"
+    },
+    {
+        id:"make",
+        name:"make",
+        type:"text",
+        placeholder:"Make",
+        label:"Make"
+    },
+    {
+        id:"year",
+        name:"year",
+        type:"number",
+        placeholder:"Year",
+        label:"Year"
+    },
+    {
+        id:"price",
+        name:"price",
+        type:"number",
+        placeholder:"Price",
+        label:"Price"
+    },
+    {
+        id:"cm3",
+        name:"cm3",
+        type:"number",
+        placeholder:"Cm3",
+        label:"Cm3"
+    },
+    {
+        id:"km",
+        name:"km",
+        type:"number",
+        placeholder:"km",
+        label:"km"
+    },
+
+]
+
+const [newCar, setNewCar] = useState(emptyCar)
+const [error, setError] = useState([])
+const navigate = useNavigate();
+
+const handleSubmit = (e) => {
+  e.preventDefault()
+  addCar(newCar)
+}
+
+
   return (
-    <div>NewCar</div>
+    <div> NewCar </div>
   )
 }
 
-export default NewCar
+export default NewCar;
