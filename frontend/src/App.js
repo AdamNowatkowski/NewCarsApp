1f0c05f0a418fcf71b6f9efc4722f50f678a8c72
Adam Nowatkowski <adam.nowatkowski@lppsa.com>
2023-05-29 15:38:59 +0200

    daisy install

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,18 @@
-import Layout from "./components/Layout";
+// import Layout from "./components/Layout";
 import React from "react";
 
 // import Lorem from "./components/Lorem";
 
 function App() {
   return (
-    <Layout>
-    <div className="App">    
-    Appka
+    // <Layout>
+    <div className="App bg-zinc-500 min-h-screen flex flex-col justify-center items-center">
+      <button class="btn btn-primary">It Works!</button>    
+
+
+    {/* Appka */}
       {/* <Lorem title="Cars FARM"/>    */}
     </div>
-    </Layout>
   );
 }
 
