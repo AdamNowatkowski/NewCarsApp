1a53b9dbce624d3d683c4714a3007bb90f40b43d
Adam Nowatkowski <adam.nowatkowski@lppsa.com>
2023-05-30 22:08:31 +0200

    progress

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import {Route, Routes} from "react-router-dom"
+import Layout from "./components/Layout";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import HomePage from "./components/HomePage";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className='App bg-zinc-500 min-h-screen flex flex-col justify-center items-center'>
+    //   <button className="btn btn-primary">App</button>
+    // </div>
+
+    <Routes>
+      <Route path="/" elementName={<Layout />} >
+        <Route path="/" elementName={<HomePage />} />
+        <Route path="/login" elementName={<Login />} />
+        <Route path="/register" elementName={<Register />} />
+      </ Route>
+    </Routes>
   );
 }
 
